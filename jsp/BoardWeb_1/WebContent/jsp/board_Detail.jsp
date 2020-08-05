28f9e4b517c4df55da97d411d798eb196e831bec
임다성 <ganjinanda12@gmail.com>
2020-08-05 17:54:36 +0900

    no message

--- jsp/BoardWeb_1/WebContent/jsp/board_Detail.jsp
+++ jsp/BoardWeb_1/WebContent/jsp/board_Detail.jsp
@@ -16,11 +16,6 @@
 		System.out.println("접속 성공!"); // static이 붙은 메소드이기 때문에 사용 가능 -> 파라미터만으로 들어오는 것만으로 작동이 가능하다
 		return con;						// static이 붙은 것은 쓰기 정말 편하다 , 클래스명인지 아는 방법 첫번째 글자가 대문자이다
 }
-
-
-String title = null;
-String ctnt = null;
-int i_student = 0;
 		
 %>
 
--- jsp/BoardWeb_2/src/com/koreait/board/BoardListSer.java
+++ jsp/BoardWeb_2/src/com/koreait/board/BoardListSer.java
@@ -1,6 +1,10 @@
 package com.koreait.board;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,8 +13,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/boardList") // /뒤까지 비워도 됨
-public class BoardListSer extends HttpServlet {
+import com.koreait.board.db.BoardDAO;
+import com.koreait.board.db.DbCon;
+import com.koreait.board.vo.BoardVO;
+
+
+@WebServlet("/boardList") // /뒤까지 비워도 됨 ()이 주소값으로 파라미터를 보냄
+public class BoardListSer extends HttpServlet { // http는 요청하고 응답하고 끊는다
 	private static final long serialVersionUID = 1L;
 
     public BoardListSer() {
@@ -21,19 +30,47 @@ public class BoardListSer extends HttpServlet {
     // object -> http -> survlet  / 호출할 때는 반대로 맨 밑부터
     
     //오버라이딩할 때 생략보다는 적어주는게 제일 좋음 클래스명이나 바꿨을 때 오류가 나기 때문에
-    @Override
+    @Override               //get방식은 화면 띄우려고 쓰는 것임
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // 여기 위에 request에서 DB작업을 한다 /
-    	String strI_board = request.getParameter("i_board");
-    	System.out.println("Servlet i_board : " + strI_board);
     	
+    	List<BoardVO> list = BoardDAO.selBoardList();
+    	request.setAttribute("data", list);
+    
     	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/boardList.jsp");
-		rd.forward(request, response);
-	} // get방식 : 쿼리스트링이 주소에 찍힌다
-	// request dispatcher는 주소값은 안바꾸는데 jsp파일로 이동한다
-	// 고객이 보낸 모든 정보는 request에 있다
+    	rd.forward(request, response);
+    	// sendredirect : 	1.
+    	//					2. 주소값 변환
+    	//					3. 무조건 get방식으로 날라감
+    	//
+    	// requestdispatcher : 3. doGet으로 보내면 get방식이 되고, doPost로 보내면 post방식이 된다
+    	//jsp파일을 연다
+    	//주소값 변환 x 
+    	// sendredirect : 1.
+    	//				  2.
+    	//				  3.
+    	// requestDispatcher
+    	
+    	try {
+    		Connection con = DbCon.getCon();
+    		PreparedStatement ps = null;
+    		ResultSet rs = null;
+    		
+    		DbCon.close(con, ps, rs);
+    		DbCon.close(con, ps);
+    	//  ~~~. 에서 .앞에 있는 것은 클래스명이다
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+		rd.forward(request, response); // request객체가 response객체를 이용해서 jsp파일의 결과물을 고객의 응답까지 리턴한다
+	} // get방식 : 쿼리스트링이 주소에 찍힌다  but get방식 안쓸거면 이 메소드 지워도 된다
+	// request dispatcher는 주소값은 안바꾸는데 jsp파일로 이동한다 
+	// 고객이 보낸 모든 정보는 request에 있다 / request는 연결고리이다
 	
+    //세션은 브라우저를 완전히 껐을 때 죽는다 , 어플리케이션은 범용이다 나머지는 개인이다
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doGet(request, response); //request는 요청이 들어오고 응답하고 나면 죽는다
 	} // post방식 : 폼으로 보내지 않는 이상 다른 것들은 get방식으로 나간다, 주소 url에 쿼리스트링이 찍히지 않는다, 대신에 많은 양을 보낼 수 있다 ex) 보통 id 이런 것들, 화면 띄울 때 많이 씀
 	
 	// jsp는 뷰 담당
