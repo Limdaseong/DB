d009495c5b693fc1720bb483cebd849d3c5b4dfb
임다성 <ganjinanda12@gmail.com>
2020-08-07 14:53:06 +0900

    no message

--- jsp/BoardWeb_2/src/com/koreait/board/BoardWriteSer.java
+++ jsp/BoardWeb_2/src/com/koreait/board/BoardWriteSer.java
@@ -23,7 +23,37 @@ public class BoardWriteSer extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String title = request.getParameter("title");
+		String ctnt = request.getParameter("ctnt");
+		String strI_student = request.getParameter("i_student");
+
+		if("".equals(title) || "".equals(ctnt) || "".equals(strI_student)) {
+			response.sendRedirect("/jsp/board_Write.jsp?err=10");
+			return;
+		}
 		
+		int i_student = Integer.parseInt(strI_student);
+		int result = -1;
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		String sql = " INSERT INTO t_board (i_board, title, ctnt, i_student) "
+				+ " SELECT nvl(max(i_board), 0)+1, ?, ?, ? "
+				+ " FROM t_board ";
+		
+		try {
+    		con = DbCon.getCon();
+    		ps = con.prepareStatement(sql);
+    		ps.setNString(1, title);
+    		ps.setNString(2, ctnt);
+    		ps.setInt(3, i_student);
+    		result = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DbCon.close(con, ps);
+		}
 		
 		String jsp = "/WEB-INF/view/boardRegMod";
 		request.getRequestDispatcher(jsp).forward(request, response);
